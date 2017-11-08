17776983f755353cc5ce7c061e83fd295f91d08f
Niklas Jørgensen <niklas220587@hotmail.com>
2017-11-08 12:35:15 +0100

    Yap

---
+++ Bibliotek/Model/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string ISBN { get; set; }
+        public string Titel { get; set; }
+        public int stock { get; set; }
+    }
+}
