483b9c6be1d7d9e954b6f957c3dddeef494e3513
Niklas Jørgensen <niklas220587@hotmail.com>
2017-11-08 13:00:44 +0100

    YAP stargate

---
+++ Bibliotek/DAL/Context.cs
@@ -0,0 +1,35 @@
+namespace DAL
+{
+    using Model;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Context : DbContext
+    {
+        // Your context has been configured to use a 'Context' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'DAL.Context' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'Context' 
+        // connection string in the application configuration file.
+        public Context()
+            : base("name=Context")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Library> Libraries{ get; set; }
+        public virtual DbSet<LoanLine> LoanLines { get; set; }
+        public virtual DbSet<Loaner> Loaners { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
