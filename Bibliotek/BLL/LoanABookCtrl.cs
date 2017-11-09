c7d733a735fbf0c54a95b584b9c2dbb7ff9481fe
Jesper Jørgensen <jesperbroge@gmail.com>
2017-11-09 10:57:41 +0100

    Added simple method for making a loan in BLL

--- Bibliotek/BLL/LoanABookCtrl.cs
+++ Bibliotek/BLL/LoanABookCtrl.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,15 @@ namespace BLL
 {
     public class LoanABookCtrl
     {
+        DALContext ctx = new DALContext();
+
+        public void MakeLoan(Loaner loaner, Book book)
+        {
+            var loanLine = new LoanLine { Loaner = loaner, Book = book };
+            loaner.LoanedBooks.Add(loanLine);
+
+            ctx.LoanLines.Add(loanLine);
+            ctx.SaveChanges();
+        }
     }
 }
