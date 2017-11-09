54367a3b28808b6dc5532036576e6e23ec55ca91
Jesper Jørgensen <jesperbroge@gmail.com>
2017-11-09 11:34:43 +0100

    AllLoaners method for loanerCtrl

--- Bibliotek/BLL/LoanerCtrl.cs
+++ Bibliotek/BLL/LoanerCtrl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace BLL
 {
     public class LoanerCtrl
     {
+        DAL.DALContext ctx = new DAL.DALContext();
+
+        public List<Loaner> AllLoaners()
+        {
+            return ctx.Loaners.ToList();
+        }
     }
+
 }
